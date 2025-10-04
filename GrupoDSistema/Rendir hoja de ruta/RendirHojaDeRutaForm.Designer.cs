60f1059993bbe4765b6132fe391b2280535acc19
Chris <Teemo@DESKTOP-2EJU9OI>
2025-10-03 22:02:08 -0300

    si

---
+++ GrupoDSistema/Rendir hoja de ruta/RendirHojaDeRutaForm.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace GrupoDSistema.Rendir_hoja_de_ruta
+{
+    partial class RendirHojaDeRutaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // RendirHojaDeRutaForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "RendirHojaDeRutaForm";
+            Text = "Rendición de hojas de ruta";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ GrupoDSistema/Rendir hoja de ruta/RendirHojaDeRutaForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoDSistema.Rendir_hoja_de_ruta
+{
+    public partial class RendirHojaDeRutaForm : Form
+    {
+        public RendirHojaDeRutaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
