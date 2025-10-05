fc86d9079108478259a45b7f6b15217782c1af0c
apanitsch <apanitsch@gmail.com>
2025-10-05 20:57:35 -0300

    Repo arreglado inicial.

---
+++ GrupoD.Tutasa/GrupoD.Tutasa/GenerarHojaDeRuta/GenerarHojaRutaForm.cs
@@ -0,0 +1,20 @@
+namespace GrupoD.Tutasa.GenerarHojaDeRuta
+{
+    public partial class GenerarHojaRutaForm : Form
+    {
+        public GenerarHojaRutaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GrupoD.Tutasa/GrupoD.Tutasa/RendirHojaDeRuta/RendirHojaDeRutaForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoD.Tutasa.RendirHojaDeRuta
+{
+    public partial class RendirHojaDeRutaForm : Form
+    {
+        public RendirHojaDeRutaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
