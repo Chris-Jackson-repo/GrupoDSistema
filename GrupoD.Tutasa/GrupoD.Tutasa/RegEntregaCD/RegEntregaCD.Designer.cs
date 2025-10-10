6d036521bd1a2a92744830bcd0c41cc2333ef2c6
Martin <martiale2002@gmail.com>
2025-10-10 00:55:40 -0300

    form registrar entrega en CD

---
+++ GrupoD.Tutasa/GrupoD.Tutasa/RegEntregaCD/RegEntregaCD.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace GrupoD.Tutasa.RegEntregaCD
+{
+    partial class RegEntregaCD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            EstEntregadoLabel = new Label();
+            EstEnCDLabel = new Label();
+            EstEnCaminoCDLabel = new Label();
+            EstadoActualLabel = new Label();
+            FechaRetiroDateTimePicker = new DateTimePicker();
+            FechaRetiroLabel = new Label();
+            CancelarButton = new Button();
+            RegistarEntregaButton = new Button();
+            MailRetiranteTextbox = new TextBox();
+            TelefonoRetiranteTextbox = new TextBox();
+            NomApRetiranteTextbox = new TextBox();
+            MailRetiranteLabel = new Label();
+            TelefonoRetiranteLabel = new Label();
+            NomApRetiranteLabel = new Label();
+            DatosRetiranteLabel = new Label();
+            FechaLlegadaAgenciaDateTimePicker = new DateTimePicker();
+            FechaLlegadaCDLabel = new Label();
+            DatosEncomiendaLabel = new Label();
+            NumeroGuiaTextbox = new TextBox();
+            NumeroGuiaLabel = new Label();
+            SuspendLayout();
+            // 
+            // EstEntregadoLabel
+            // 
+            EstEntregadoLabel.AutoSize = true;
+            EstEntregadoLabel.Font = new Font("Segoe UI", 8F);
+            EstEntregadoLabel.Location = new Point(342, 157);
+            EstEntregadoLabel.Name = "EstEntregadoLabel";
+            EstEntregadoLabel.Size = new Size(72, 19);
+            EstEntregadoLabel.TabIndex = 45;
+            EstEntregadoLabel.Text = "Entregado";
+            // 
+            // EstEnCDLabel
+            // 
+            EstEnCDLabel.AutoSize = true;
+            EstEnCDLabel.Font = new Font("Segoe UI", 8F);
+            EstEnCDLabel.Location = new Point(220, 157);
+            EstEnCDLabel.Name = "EstEnCDLabel";
+            EstEnCDLabel.Size = new Size(47, 19);
+            EstEnCDLabel.TabIndex = 44;
+            EstEnCDLabel.Text = "En CD";
+            // 
+            // EstEnCaminoCDLabel
+            // 
+            EstEnCaminoCDLabel.AutoSize = true;
+            EstEnCaminoCDLabel.Font = new Font("Segoe UI", 8F);
+            EstEnCaminoCDLabel.Location = new Point(34, 157);
+            EstEnCaminoCDLabel.Name = "EstEnCaminoCDLabel";
+            EstEnCaminoCDLabel.Size = new Size(109, 19);
+            EstEnCaminoCDLabel.TabIndex = 43;
+            EstEnCaminoCDLabel.Text = "En camino al CD";
+            // 
+            // EstadoActualLabel
+            // 
+            EstadoActualLabel.AutoSize = true;
+            EstadoActualLabel.Font = new Font("Segoe UI", 12F);
+            EstadoActualLabel.Location = new Point(26, 112);
+            EstadoActualLabel.Name = "EstadoActualLabel";
+            EstadoActualLabel.Size = new Size(128, 28);
+            EstadoActualLabel.TabIndex = 42;
+            EstadoActualLabel.Text = "Estado actual";
+            // 
+            // FechaRetiroDateTimePicker
+            // 
+            FechaRetiroDateTimePicker.Font = new Font("Segoe UI", 8F);
+            FechaRetiroDateTimePicker.Location = new Point(220, 283);
+            FechaRetiroDateTimePicker.Name = "FechaRetiroDateTimePicker";
+            FechaRetiroDateTimePicker.Size = new Size(292, 25);
+            FechaRetiroDateTimePicker.TabIndex = 41;
+            // 
+            // FechaRetiroLabel
+            // 
+            FechaRetiroLabel.AutoSize = true;
+            FechaRetiroLabel.Font = new Font("Segoe UI", 8F);
+            FechaRetiroLabel.Location = new Point(34, 283);
+            FechaRetiroLabel.Name = "FechaRetiroLabel";
+            FechaRetiroLabel.Size = new Size(100, 19);
+            FechaRetiroLabel.TabIndex = 40;
+            FechaRetiroLabel.Text = "Fecha de retiro";
+            // 
+            // CancelarButton
+            // 
+            CancelarButton.Location = new Point(655, 532);
+            CancelarButton.Name = "CancelarButton";
+            CancelarButton.Size = new Size(134, 30);
+            CancelarButton.TabIndex = 39;
+            CancelarButton.Text = "Cancelar";
+            CancelarButton.UseVisualStyleBackColor = true;
+            // 
+            // RegistarEntregaButton
+            // 
+            RegistarEntregaButton.Location = new Point(515, 532);
+            RegistarEntregaButton.Name = "RegistarEntregaButton";
+            RegistarEntregaButton.Size = new Size(134, 30);
+            RegistarEntregaButton.TabIndex = 38;
+            RegistarEntregaButton.Text = "Registar Entrega";
+            RegistarEntregaButton.UseVisualStyleBackColor = true;
+            // 
+            // MailRetiranteTextbox
+            // 
+            MailRetiranteTextbox.Font = new Font("Segoe UI", 8F);
+            MailRetiranteTextbox.Location = new Point(220, 460);
+            MailRetiranteTextbox.Name = "MailRetiranteTextbox";
+            MailRetiranteTextbox.Size = new Size(292, 25);
+            MailRetiranteTextbox.TabIndex = 37;
+            // 
+            // TelefonoRetiranteTextbox
+            // 
+            TelefonoRetiranteTextbox.Font = new Font("Segoe UI", 8F);
+            TelefonoRetiranteTextbox.Location = new Point(220, 421);
+            TelefonoRetiranteTextbox.Name = "TelefonoRetiranteTextbox";
+            TelefonoRetiranteTextbox.Size = new Size(292, 25);
+            TelefonoRetiranteTextbox.TabIndex = 36;
+            // 
+            // NomApRetiranteTextbox
+            // 
+            NomApRetiranteTextbox.Font = new Font("Segoe UI", 8F);
+            NomApRetiranteTextbox.Location = new Point(220, 379);
+            NomApRetiranteTextbox.Name = "NomApRetiranteTextbox";
+            NomApRetiranteTextbox.Size = new Size(292, 25);
+            NomApRetiranteTextbox.TabIndex = 35;
+            // 
+            // MailRetiranteLabel
+            // 
+            MailRetiranteLabel.AutoSize = true;
+            MailRetiranteLabel.Font = new Font("Segoe UI", 8F);
+            MailRetiranteLabel.Location = new Point(34, 460);
+            MailRetiranteLabel.Name = "MailRetiranteLabel";
+            MailRetiranteLabel.Size = new Size(35, 19);
+            MailRetiranteLabel.TabIndex = 34;
+            MailRetiranteLabel.Text = "Mail";
+            // 
+            // TelefonoRetiranteLabel
+            // 
+            TelefonoRetiranteLabel.AutoSize = true;
+            TelefonoRetiranteLabel.Font = new Font("Segoe UI", 8F);
+            TelefonoRetiranteLabel.Location = new Point(34, 421);
+            TelefonoRetiranteLabel.Name = "TelefonoRetiranteLabel";
+            TelefonoRetiranteLabel.Size = new Size(60, 19);
+            TelefonoRetiranteLabel.TabIndex = 33;
+            TelefonoRetiranteLabel.Text = "Teléfono";
+            // 
+            // NomApRetiranteLabel
+            // 
+            NomApRetiranteLabel.AutoSize = true;
+            NomApRetiranteLabel.Font = new Font("Segoe UI", 8F);
+            NomApRetiranteLabel.Location = new Point(34, 379);
+            NomApRetiranteLabel.Name = "NomApRetiranteLabel";
+            NomApRetiranteLabel.Size = new Size(123, 19);
+            NomApRetiranteLabel.TabIndex = 32;
+            NomApRetiranteLabel.Text = "Nombre y Apellido";
+            // 
+            // DatosRetiranteLabel
+            // 
+            DatosRetiranteLabel.AutoSize = true;
+            DatosRetiranteLabel.Font = new Font("Segoe UI", 12F);
+            DatosRetiranteLabel.Location = new Point(26, 331);
+            DatosRetiranteLabel.Name = "DatosRetiranteLabel";
+            DatosRetiranteLabel.Size = new Size(174, 28);
+            DatosRetiranteLabel.TabIndex = 31;
+            DatosRetiranteLabel.Text = "Datos del retirante";
+            // 
+            // FechaLlegadaAgenciaDateTimePicker
+            // 
+            FechaLlegadaAgenciaDateTimePicker.Font = new Font("Segoe UI", 8F);
+            FechaLlegadaAgenciaDateTimePicker.Location = new Point(220, 239);
+            FechaLlegadaAgenciaDateTimePicker.Name = "FechaLlegadaAgenciaDateTimePicker";
+            FechaLlegadaAgenciaDateTimePicker.Size = new Size(292, 25);
+            FechaLlegadaAgenciaDateTimePicker.TabIndex = 30;
+            // 
+            // FechaLlegadaCDLabel
+            // 
+            FechaLlegadaCDLabel.AutoSize = true;
+            FechaLlegadaCDLabel.Font = new Font("Segoe UI", 8F);
+            FechaLlegadaCDLabel.Location = new Point(34, 239);
+            FechaLlegadaCDLabel.Name = "FechaLlegadaCDLabel";
+            FechaLlegadaCDLabel.Size = new Size(144, 19);
+            FechaLlegadaCDLabel.TabIndex = 29;
+            FechaLlegadaCDLabel.Text = "Fecha de llegada a CD";
+            // 
+            // DatosEncomiendaLabel
+            // 
+            DatosEncomiendaLabel.AutoSize = true;
+            DatosEncomiendaLabel.Font = new Font("Segoe UI", 12F);
+            DatosEncomiendaLabel.Location = new Point(26, 194);
+            DatosEncomiendaLabel.Name = "DatosEncomiendaLabel";
+            DatosEncomiendaLabel.Size = new Size(222, 28);
+            DatosEncomiendaLabel.TabIndex = 28;
+            DatosEncomiendaLabel.Text = "Datos de la encomienda";
+            // 
+            // NumeroGuiaTextbox
+            // 
+            NumeroGuiaTextbox.Font = new Font("Segoe UI", 8F);
+            NumeroGuiaTextbox.Location = new Point(34, 63);
+            NumeroGuiaTextbox.Name = "NumeroGuiaTextbox";
+            NumeroGuiaTextbox.Size = new Size(309, 25);
+            NumeroGuiaTextbox.TabIndex = 27;
+            // 
+            // NumeroGuiaLabel
+            // 
+            NumeroGuiaLabel.AutoSize = true;
+            NumeroGuiaLabel.Font = new Font("Segoe UI", 12F);
+            NumeroGuiaLabel.Location = new Point(26, 32);
+            NumeroGuiaLabel.Name = "NumeroGuiaLabel";
+            NumeroGuiaLabel.Size = new Size(154, 28);
+            NumeroGuiaLabel.TabIndex = 26;
+            NumeroGuiaLabel.Text = "Número de guía";
+            // 
+            // RegEntregaCD
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(787, 567);
+            Controls.Add(EstEntregadoLabel);
+            Controls.Add(EstEnCDLabel);
+            Controls.Add(EstEnCaminoCDLabel);
+            Controls.Add(EstadoActualLabel);
+            Controls.Add(FechaRetiroDateTimePicker);
+            Controls.Add(FechaRetiroLabel);
+            Controls.Add(CancelarButton);
+            Controls.Add(RegistarEntregaButton);
+            Controls.Add(MailRetiranteTextbox);
+            Controls.Add(TelefonoRetiranteTextbox);
+            Controls.Add(NomApRetiranteTextbox);
+            Controls.Add(MailRetiranteLabel);
+            Controls.Add(TelefonoRetiranteLabel);
+            Controls.Add(NomApRetiranteLabel);
+            Controls.Add(DatosRetiranteLabel);
+            Controls.Add(FechaLlegadaAgenciaDateTimePicker);
+            Controls.Add(FechaLlegadaCDLabel);
+            Controls.Add(DatosEncomiendaLabel);
+            Controls.Add(NumeroGuiaTextbox);
+            Controls.Add(NumeroGuiaLabel);
+            Name = "RegEntregaCD";
+            Text = "RegEntregaCD";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label EstEntregadoLabel;
+        private Label EstEnCDLabel;
+        private Label EstEnCaminoCDLabel;
+        private Label EstadoActualLabel;
+        private DateTimePicker FechaRetiroDateTimePicker;
+        private Label FechaRetiroLabel;
+        private Button CancelarButton;
+        private Button RegistarEntregaButton;
+        private TextBox MailRetiranteTextbox;
+        private TextBox TelefonoRetiranteTextbox;
+        private TextBox NomApRetiranteTextbox;
+        private Label MailRetiranteLabel;
+        private Label TelefonoRetiranteLabel;
+        private Label NomApRetiranteLabel;
+        private Label DatosRetiranteLabel;
+        private DateTimePicker FechaLlegadaAgenciaDateTimePicker;
+        private Label FechaLlegadaCDLabel;
+        private Label DatosEncomiendaLabel;
+        private TextBox NumeroGuiaTextbox;
+        private Label NumeroGuiaLabel;
+    }
+}
