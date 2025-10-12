d621baf817acd1e4f94292e15b4a2822caa0730d
marianofioretti9-a11y <marianofioretti9@gmail.com>
2025-10-11 21:26:00 -0300

    filas

--- GrupoD.Tutasa/GrupoD.Tutasa/ReporteCostosVentas/Form2.Designer.cs
+++ GrupoD.Tutasa/GrupoD.Tutasa/ReporteCostosVentas/Form2.Designer.cs
@@ -42,6 +42,22 @@
             Column3 = new DataGridViewTextBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             Column5 = new DataGridViewTextBoxColumn();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)Resultados).BeginInit();
             SuspendLayout();
             // 
@@ -129,11 +145,11 @@
             Resultados.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
             Resultados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             Resultados.Columns.AddRange(new DataGridViewColumn[] { Column1, Column3, Column4, Column5 });
-            Resultados.Location = new Point(59, 310);
+            Resultados.Location = new Point(73, 283);
             Resultados.Name = "Resultados";
             Resultados.RowHeadersWidth = 51;
             Resultados.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            Resultados.Size = new Size(856, 253);
+            Resultados.Size = new Size(842, 224);
             Resultados.TabIndex = 28;
             Resultados.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -161,11 +177,139 @@
             Column5.MinimumWidth = 6;
             Column5.Name = "Column5";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(708, 338);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 27);
+            textBox1.TabIndex = 29;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(513, 338);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(199, 27);
+            textBox2.TabIndex = 30;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(310, 315);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(220, 27);
+            textBox3.TabIndex = 31;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(73, 315);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(241, 27);
+            textBox4.TabIndex = 32;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(708, 362);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(207, 27);
+            textBox5.TabIndex = 33;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(708, 386);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(207, 27);
+            textBox6.TabIndex = 34;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(513, 362);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(199, 27);
+            textBox7.TabIndex = 35;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(512, 386);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(200, 27);
+            textBox8.TabIndex = 36;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(708, 315);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(207, 27);
+            textBox9.TabIndex = 37;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(513, 315);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(199, 27);
+            textBox10.TabIndex = 38;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(310, 338);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(206, 27);
+            textBox11.TabIndex = 39;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(310, 362);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(206, 27);
+            textBox12.TabIndex = 40;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(311, 386);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(205, 27);
+            textBox13.TabIndex = 41;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(73, 338);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(241, 27);
+            textBox14.TabIndex = 42;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(73, 362);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(241, 27);
+            textBox15.TabIndex = 43;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(73, 386);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(241, 27);
+            textBox16.TabIndex = 44;
+            // 
             // ReporteDeCostosvsVenta
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(998, 601);
+            Controls.Add(textBox16);
+            Controls.Add(textBox15);
+            Controls.Add(textBox14);
+            Controls.Add(textBox13);
+            Controls.Add(textBox12);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(Resultados);
             Controls.Add(Cancelar);
             Controls.Add(EmpresaTransporte);
@@ -200,5 +344,21 @@
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox16;
     }
 }
