dc3e08f14f5bb419509b98b0605a4e712faf9721
Martin <martiale2002@gmail.com>
2025-10-21 14:54:10 -0300

    creacion carpeta almacenes + almacen de guias

---
+++ GrupoD.Tutasa/GrupoD.Tutasa/Almacenes/GuiasAlmacen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoD.Tutasa.Almacenes
+{
+    internal class GuiasAlmacen
+    {
+        /* 
+        static class GuiasAlmacen
+        {
+            static List<string> Guia = new List<Guia>();
+            static GuiasAlmacen()
+            {
+                if (File.Exists("Guias.json"))
+                {
+                    var guiaJson = File.ReadAllText("Guias.json");
+                    Guia = System.Text.Json.JsonSerializer.Deserialize<List<Guia>>(guiaJson) ?? new List<Guia>();
+                }
+            }
+            public static void Grabar()
+            {
+                var guiaJson = System.Text.Json.JsonSerializer.Serialize(Guia);
+                File.WriteAllText("Guias.json", guiaJson);
+            }
+
+
+        }
+        */
+        // En el program para guardar datos del almacen: GuiasAlmacen.Grabar();
+    }
